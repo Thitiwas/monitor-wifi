4527dfee5cc28689bef8baea2909f83753bc6b0f
U-DESKTOP-V02D53O\champ <you@example.com>
2017-05-11 18:54:56 +0700

    Successful 1

---
+++ server.js
@@ -0,0 +1,117 @@
+/* eslint-disable */
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var cors = require('cors')
+const scanner = require('node-wifi-scanner')
+var snmp = require('snmp-native')
+var community = 'public'
+var humanizeDuration = require('humanize-duration')
+
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('dist'))
+
+app.set('port', (process.env.PORT || 4000))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+var int_415 = []
+var port_415 = []
+var time_415 = []
+var inbound = []
+var outbound = []
+var data415 = []
+
+app.get('/AP', function (req, res) {
+  var getintR415 = new snmp.Session({ host: '10.41.160.1', community: community })
+  var oidget_int = '.1.3.6.1.2.1.2.2.1.2'
+  getintR415.getSubtree({ oid: oidget_int }, function (err, varbinds) {
+    varbinds.forEach(function (data) {
+      int_415.push(data.value)
+    })
+    getintR415.close()
+  })
+// /////////////////////////////////////////////////////////////////////////////////////////////
+  var getportR415 = new snmp.Session({ host: '10.41.160.1', community: community })
+  var oidget_port = '.1.3.6.1.2.1.2.2.1.8'
+  getportR415.getSubtree({ oid: oidget_port }, function (err, varbinds) {
+    varbinds.forEach(function (data) {
+      port_415.push(updown(data.value))
+    })
+    getportR415.close()
+  })
+// /////////////////////////////////////////////////////////////////////////////////////////////
+  var gettimeR415 = new snmp.Session({ host: '10.41.160.1', community: community })
+  var oidget_time = '.1.3.6.1.2.1.2.2.1.9'
+  gettimeR415.getSubtree({ oid: oidget_time }, function (err, varbinds) {
+    varbinds.forEach(function (data) {
+      time_415.push(humanizeDuration(data.value))
+    })
+    gettimeR415.close()
+  })
+// /////////////////////////////////////////////////////////////////////////////////////////////
+  var getinbound = new snmp.Session({ host: '10.41.160.1', community: community })
+  var oidget_inbound = '.1.3.6.1.2.1.2.2.1.10'
+  getinbound.getSubtree({ oid: oidget_inbound }, function (err, varbinds) {
+    varbinds.forEach(function (data) {
+      inbound.push(convert(data.value))
+    })
+    getinbound.close()
+  })
+// /////////////////////////////////////////////////////////////////////////////////////////////
+  var getoutbound = new snmp.Session({ host: '10.41.160.1', community: community })
+  var oidget_outbound = '.1.3.6.1.2.1.2.2.1.16'
+  getoutbound.getSubtree({ oid: oidget_outbound }, function (err, varbinds) {
+    varbinds.forEach(function (data) {
+      outbound.push(convert(data.value))
+    })
+    getoutbound.close()
+  })
+// /////////////////////////////////////////////////////////////////////////////////////////////
+  int_415.forEach(function (err, index) {
+    var set = {
+      int: int_415[index],
+      port: port_415[index],
+      time: time_415[index],
+      inbound : inbound[index],
+      outbound : outbound[index]
+    }
+    data415.push(set)
+  })
+  int_415 = []
+  port_415 = []
+  time_415 = []
+  inbound = []
+  outbound = []
+  res.send(data415)
+  data415 = []
+})
+// /////////////////////////////////////////////////////////////////////////////////////////////
+app.get('/scan', function (req, res) {
+  scanner.scan((err, networks) => {
+    if (err) console.error(err)
+    res.send(networks)
+  })
+})
+function convert (byte) {
+   var sizes = ['Bytes', 'Kbps', 'Mbps', 'Gbps', 'Tbps']
+   byte = byte * 8
+   if (byte == 0) return '0 Byte'
+   var i = parseFloat(Math.floor(Math.log(byte) / Math.log(1000)))
+   return parseFloat(byte / Math.pow(1000, i), 2).toFixed(2) + ' ' + sizes[i]
+}
+
+function updown (status) {
+  if(status == '1') {
+    return "up"
+  }
+  else if (status == '2') {
+    return "Down"
+  }
+}
+
+app.listen(app.get('port'), function () {
+  console.log('run at port', app.get('port'))
+})
---
+++ src/components/Hello.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="hello">
+    <div class="bar">
+      <br>
+      <h2> &nbsp;&nbsp;&nbsp; FITM WIFI Monitoring</h2>
+    </div>
+    <div class="left">
+      <h1>Scanwifi</h1><hr>
+      <div v-for="show in data">
+        <h4>name :         {{show.ssid}}        </h4>
+        <h4>mac Address :  {{show.mac}}         </h4>
+        <progress :value="(100+show.rssi)" max="100" class="rssi"></progress>
+        <h4>rssi :         {{show.rssi}}     dBm</h4>
+        <h4>Channel :      {{show.channel}}     </h4>
+        <hr>
+      </div>
+    </div>
+    <div class="right">
+      <h1>Monitor Access Point</h1>
+      AP1: <B>{{data2[40].port}}</B>&emsp;AP2: <B>{{data2[41].port}}</B>&emsp;AP3: <B>{{data2[42].port}}</B>&emsp;
+      AP4: <B>{{data2[43].port}}</B>&emsp;AP5: <B>{{data2[44].port}}</B>&emsp;AP6: <B>{{data2[45].port}}</B>&emsp;AP7: <B>{{data2[46].port}}</B>&emsp;AP8: <B>{{data2[47].port}}</B><hr>
+      <div>
+        <div class="content">
+          <h2>AP1</h2><br>
+          Port:&emsp;&emsp;{{data2[40].int}} <br>
+          Status:&emsp;{{data2[40].port}} <br>
+          UpTime:&emsp; {{data2[40].time}} <br>
+          Inbound:&emsp;{{data2[40].inbound}} <br>
+          Outbound: {{data2[40].outbound}} <br><hr>
+        </div>
+        <div class="content">
+          <h2>AP2</h2><br>
+          Port:&emsp;&emsp;{{data2[41].int}} <br>
+          Status:&emsp;{{data2[41].port}} <br>
+          UpTime:&emsp; {{data2[41].time}} <br>
+          Inbound:&emsp;{{data2[41].inbound}} <br>
+          Outbound: {{data2[41].outbound}} <br><hr>
+        </div>
+        <div class="content">
+          <h2>AP3</h2><br>
+          Port:&emsp;&emsp;{{data2[42].int}} <br>
+          Status:&emsp;{{data2[42].port}} <br>
+          UpTime:&emsp; {{data2[42].time}} <br>
+          Inbound:&emsp;{{data2[42].inbound}} <br>
+          Outbound: {{data2[42].outbound}} <br><hr>
+        </div>
+        <div class="content">
+          <h2>AP4</h2><br>
+          Port:&emsp;&emsp;{{data2[43].int}} <br>
+          Status:&emsp;{{data2[43].port}} <br>
+          UpTime:&emsp; {{data2[43].time}} <br>
+          Inbound:&emsp;{{data2[43].inbound}} <br>
+          Outbound: {{data2[43].outbound}} <br><hr>
+        </div>
+        <div class="content">
+          <h2>AP5</h2><br>
+          Port:&emsp;&emsp;{{data2[44].int}} <br>
+          Status:&emsp;{{data2[44].port}} <br>
+          UpTime:&emsp; {{data2[44].time}} <br>
+          Inbound:&emsp;{{data2[44].inbound}} <br>
+          Outbound: {{data2[44].outbound}} <br><hr>
+        </div>
+        <div class="content">
+          <h2>AP6</h2><br>
+          Port:&emsp;&emsp;{{data2[45].int}} <br>
+          Status:&emsp;{{data2[45].port}} <br>
+          UpTime:&emsp; {{data2[45].time}} <br>
+          Inbound:&emsp;{{data2[45].inbound}} <br>
+          Outbound: {{data2[45].outbound}} <br><hr>
+        </div>
+        <div class="content">
+          <h2>AP7</h2><br>
+          Port:&emsp;&emsp;{{data2[46].int}} <br>
+          Status:&emsp;{{data2[46].port}} <br>
+          UpTime:&emsp; {{data2[46].time}} <br>
+          Inbound:&emsp;{{data2[46].inbound}} <br>
+          Outbound: {{data2[46].outbound}} <br><hr>
+        </div>
+        <div class="content">
+          <h2>AP8</h2><br>
+          Port:&emsp;&emsp;{{data2[47].int}} <br>
+          Status:&emsp;{{data2[47].port}} <br>
+          UpTime:&emsp; {{data2[47].time}} <br>
+          Inbound:&emsp;{{data2[47].inbound}} <br>
+          Outbound: {{data2[47].outbound}} <br><hr>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      data: [],
+      data2: []
+    }
+  },
+  mounted () {
+    var vm = this
+    setInterval(function () {
+      axios.get('http://localhost:4000/scan').then(res => {
+        vm.data = res.data
+      })
+      axios.get('http://localhost:4000/AP').then(res => {
+        vm.data2 = res.data
+      })
+    }, 5000)
+    console.log(vm.data2)
+  },
+}
+</script>
+
+<style>
+  .bar {
+    color: white;
+    height: 15vh;
+    background-color: #01579b;
+    margin-bottom: 5vh;
+  }
+  .left {
+    float: left;
+    width: 25%;
+    padding-left: 2%
+  }
+  .right {
+    float: left;
+    width: 70%;
+    padding-left: 3%;
+  }
+  .content {
+    float: left;
+    width: 49%;
+    /*padding-left: %;*/
+  }
+.rssi {
+  background-color: blue;
+}
+</style>
